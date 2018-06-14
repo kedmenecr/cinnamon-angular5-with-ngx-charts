d67a807b41ef80cac3a4b6d5434f161c875f1b9e
Roberto <kedmenec.roberto@gmail.com>
2018-06-14 13:31:15 +0200

    Add the code for chart.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+// data goes here
+  single: any[];
+  multi: any[] = [
+  {
+    name: 'Cyan',
+    series: [
+      {
+        name: 5,
+        value: 2650
+      },
+      {
+        name: 10,
+        value: 2800      },
+      {
+        name: 15,
+        value: 2000
+      }
+    ]
+  },
+  {
+    name: 'Yellow',
+    series: [
+      {
+        name: 5,
+        value: 2500
+      },
+      {
+        name: 10,
+        value: 3100
+      },
+      {
+        name: 15,
+        value: 2350
+      }
+    ]
+  }
+];
+
+  view: any[] = [700, 400];
+
+  // options for the chart
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Number';
+  showYAxisLabel = true;
+  yAxisLabel = 'Color Value';
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
 }
+
